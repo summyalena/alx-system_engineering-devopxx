af78397efcb07ce6a235f57b5b9d6d143dc3839b
lroudge <lauradrc@hotmail.fr>
2019-08-08 21:40:33 +0000

    change the after attribute

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -6,7 +6,7 @@ package { 'nginx':
 file_line { 'Insert the redirection line in the config file':
   ensure   => 'present',
   path     => '/etc/nginx/sites-available/default',
-  after    => 'server_name _;',
+  after    => 'listen 80 default_server;',
   line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=wpV-gGA4PSk permanent;',
 }
 
