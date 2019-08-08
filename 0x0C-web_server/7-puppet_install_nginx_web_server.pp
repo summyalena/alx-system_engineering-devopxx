4d8802bb41c47271a1737d9cd4619b9a2643c03a
lroudge <lauradrc@hotmail.fr>
2019-08-08 21:49:39 +0000

    puppet lint on 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,10 +4,10 @@ package { 'nginx':
 }
 
 file_line { 'Insert the redirection line in the config file':
-  ensure   => 'present',
-  path     => '/etc/nginx/sites-available/default',
-  after    => 'listen 80 default_server;',
-  line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=wpV-gGA4PSk permanent;',
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=wpV-gGA4PSk permanent;',
 }
 
 file { '/var/www/html/index.html':
