506246151be01adc536a2cb379a8f18184276513
lroudge <lauradrc@hotmail.fr>
2019-08-08 20:34:48 +0000

    Add task 7

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,17 @@
+# installs and configures nginx
+package { 'Install nginx':
+  ensure => installed,
+  name   => 'nginx',
+}
+
+file { 'Create the default page returned by curl':
+  content => 'Holberton School',
+  path    => '/var/www/html/index.html',
+}
+
+file_line { 'Insert the redirection line in the config file':
+  ensure => present,
+  after  => 'server_name _;'
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=wpV-gGA4PSk permanent;',
+  path   => '/etc/nginx/sites-available/default',
+}
