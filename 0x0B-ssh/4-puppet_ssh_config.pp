93596b39d547c95086d679a5560d620b4566874f
lroudge <lauradrc@hotmail.fr>
2019-08-09 00:07:58 +0000

    Change the titles in task 4 to their right position

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # sets up a client SSH configuration file so that we can connect to a server without using a password
 include stdlib
-file_line { 'Turn off passwd auth':
+file_line { 'Declare identity file':
   path    => '/etc/ssh/ssh_config',
   line    => '    IdentityFile ~/.ssh/holberton',
   replace => true,
 }
 
-file_line { 'Declare identity file':
+file_line { 'Turn off passwd auth':
   path    => '/etc/ssh/ssh_config',
   line    => '    PasswordAuthentication no',
   replace => true,
