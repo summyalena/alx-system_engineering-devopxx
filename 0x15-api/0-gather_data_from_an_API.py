e6a347020432e40b9ca409edc08e51029570aa85
lroudge <lauradrc@hotmail.fr>
2019-09-17 01:28:38 +0000

    Add missing colon in task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
             if task.get('completed'):
                 completed += 1
 
-    print('Employee {} is done with tasks({}/{})'
+    print('Employee {} is done with tasks({}/{}):'
           .format(name, completed, totalTasks))
 
     print('\n'.join(["\t " + task.get('title') for task in todos.json()
